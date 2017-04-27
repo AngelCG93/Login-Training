dc2c9af018d86ec51af95081426ec9c393fe9961
AngelCG93 <enyor_angel@hotmail.com>
2017-04-27 14:24:32 -0700

    added gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,17 @@
 var gulp = require('gulp');
 var tinylr = require('tiny-lr');
+
+gulp.task('watch', () => {
+  var lr = tinylr();
+  lr.listen(35729, () => {
+    console.log('... LiveReload on localhost:35729\n');
+  });
+  gulp.watch(['index.html'], evt => {
+    console.log(evt.path);
+    lr.changed({
+      body: {
+        files: [evt.path]
+      }
+    });
+  });
+});
