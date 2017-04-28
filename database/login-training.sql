6a0794f564ded7fe2e63bc5165de00b851688345
AngelCG93 <enyor_angel@hotmail.com>
2017-04-28 12:42:30 -0700

    changed password to not null

--- database/login-training.sql
+++ database/login-training.sql
@@ -2,10 +2,10 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id`      int(11)     NOT NULL AUTO_INCREMENT,
-  `username`     varchar(50) NOT NULL UNIQUE,
-  `password`     varchar(50) NOT NULL UNIQUE,
-  `user_created` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_updated` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id`       int(11)     NOT NULL AUTO_INCREMENT,
+  `user_name`     varchar(50) NOT NULL UNIQUE,
+  `user_password` varchar(50) NOT NULL,
+  `user_created`  datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_updated`  datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
