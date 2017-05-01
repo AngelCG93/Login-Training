e6aec963361966cb8171115f766b68f3f045c744
AngelCG93 <enyor_angel@hotmail.com>
2017-05-01 16:58:10 -0700

    TESTED

--- login-training.php
+++ login-training.php
@@ -17,11 +17,15 @@ function getConnection(){
   $dbh->exec("set names utf8");
   return $dbh;
 };
+
 //------------------------------------------------------------------------------
+
 $app = new \Slim\Slim();
 //------------------------------------------------------------------------------
 // GET
 $app->get('/hello_world', 'hello_world');
+$app->get('/get_user/:user', 'get_user');
+$app->get('/get_answer/:user/:password','get_answer');
 //------------------------------------------------------------------------------
 // POST
 //------------------------------------------------------------------------------
@@ -37,6 +41,20 @@ function hello_world() {
   $sql = "SELECT * FROM users";
   $result = query_get($sql);
   echo json_encode($result, JSON_UNESCAPED_UNICODE);
+};
+function get_user($user) {
+  $sql = "SELECT * FROM `users` WHERE `user_name`='$user'";
+  $result = query_get($sql);
+  echo json_encode($result);
+};
+function get_answer($user, $password) {
+  $sql = "SELECT * FROM `users` WHERE `user_name`='$user' AND `user_password`='$password'";
+  $result = query_get($sql);
+  if ($result) {    /*echo "{'loginStatus': '".($result ? 1 : 0)."'}";*/
+    echo "{'loginStatus': '1'}";
+  } else {
+    echo "{'loginStatus': '0'}";
+  }
 };
 //------------------------------------------------------------------------------
 // POST functions
